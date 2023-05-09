b3dd7d08f2e631dad0e98f575dde1e68017189a0
Oscar Fonseca <edsphinx@gmail.com>
2023-05-09 00:59:07 -0600

    iframe reset before posting code

--- src/index.tsx
+++ src/index.tsx
@@ -26,6 +26,8 @@ const App = () => {
 			return;
 		}
 
+		iframe.current.srcdoc = html;
+
 		const result = await ref.current.build({
 			entryPoints: ['index.js'],
 			bundle: true,
@@ -47,7 +49,13 @@ const App = () => {
 				<div id="root"></div>
 				<script>
 					window.addEventListener('message', e => {
-						eval(e.data);
+						try {
+							eval(e.data);
+						} catch (err) {
+							const root = document.querySelector('#root');
+							root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+							console.log(err);
+						}
 					}, false);
 				</script>
 			</body>
