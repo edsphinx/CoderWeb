aa1ee7c6ff0e7bd18b079969450e026fdeb49cce
Oscar Fonseca <edsphinx@gmail.com>
2023-05-09 00:45:32 -0600

    iframe for code execution added

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
 	const ref = useRef<any>();
+	const iframe = useRef<any>();
 	const [input, setInput] = useState('');
 	const [code, setCode] = useState('');
 
@@ -36,8 +37,22 @@ const App = () => {
 			},
 		});
 
-		setCode(result.outputFiles[0].text);
+		iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
 	};
+
+	const html = `
+		<html>
+			<head></head>
+			<body>
+				<div id="root"></div>
+				<script>
+					window.addEventListener('message', e => {
+						eval(e.data);
+					}, false);
+				</script>
+			</body>
+		</html>
+	`;
 
 	return (
 		<div>
@@ -49,6 +64,12 @@ const App = () => {
 				<button onClick={onClick}>Submit</button>
 			</div>
 			<pre>{code}</pre>
+			<iframe
+				ref={iframe}
+				title='codeIframe'
+				sandbox='allow-scripts'
+				srcDoc={html}
+			/>
 		</div>
 	);
 };
