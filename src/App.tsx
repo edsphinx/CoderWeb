ec36184934f07b364c639d3a112d1a21d70732eb
Oscar Fonseca <edsphinx@gmail.com>
2023-05-09 21:22:29 -0600

    Refactor Logic into Bundle and App

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import CodeEditor from './components/code-editor';
+import Preview from './components/preview';
+import bundle from './bundler';
+
+const App = () => {
+	const [code, setCode] = useState('');
+	const [input, setInput] = useState('');
+
+	const onClick = async () => {
+		const output = await bundle(input);
+		setCode(output);
+	};
+
+	return (
+		<div>
+			<CodeEditor
+				initialValue='const a = 100;'
+				onChange={(value) => setInput(value)}
+			/>
+			<div>
+				<button onClick={onClick}>Submit</button>
+			</div>
+			<Preview code={code} />
+		</div>
+	);
+};
+
+export default App;
